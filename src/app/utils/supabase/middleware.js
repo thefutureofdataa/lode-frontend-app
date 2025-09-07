c71b3a81135eaa20b241e1076deffcd555ef80be
john <ypoh007@e.ntu.edu.sg>
2025-09-07 16:13:22 +0800

    Updated the correct path.

--- src/app/utils/supabase/middleware.js
+++ src/app/utils/supabase/middleware.js
@@ -39,13 +39,13 @@ export async function updateSession(request) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/auth/login') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
     !request.nextUrl.pathname.startsWith('/error')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    url.pathname = '/auth/login'
     return NextResponse.redirect(url)
   }
 
